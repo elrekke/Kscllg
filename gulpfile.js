22f013f2a3018646812daa5966dbe716da22b57e
elrekke <elrekke@gmail.com>
2019-05-03 21:00:50 +0300

    all gulp settings are done

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,28 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
-  
-  exports.default = defaultTask
+const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+var gulps = require("gulp-series");
+const { series } = require('gulp');
+
+
+gulp.task('minifyhtml', () => {
+  return gulp.src('src/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('move-js', () => {
+  return gulp.src('./src/js/*.js')
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('fonts', () => {
+  return gulp.src('./src/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'));
+});
+
+
+
+gulp.task('default', gulp.series("minifyhtml", "move-js", "fonts", function (done) {
+
+  done();
+}));
